ce12699559340743740f6db4bff6819fc9338e59
sio2000 <karapanosnikos9@gmail.com>
2025-10-12 19:04:06 -0700

    Διόρθωση Bundle ID και Reference Name - ταίριασμα με το provisioning profile

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.siozostheoharis.getfit',
+  appId: 'com.slozostheoharis.getfit',
   appName: 'GetFit',
   webDir: 'dist',
   server: {
